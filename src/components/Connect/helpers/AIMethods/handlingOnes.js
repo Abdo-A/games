963d45ca1768e0d251eb6570285c745b2c5d8617
Abdo-A <abdopoetry@gmail.com>
2018-10-02 23:04:38 +0200

    Improved handlingOneHorizontal AI method

--- src/components/Connect/helpers/AIMethods/handlingOnes.js
+++ src/components/Connect/helpers/AIMethods/handlingOnes.js
@@ -2,29 +2,31 @@ export const horizontalOne = gameState => {
   for (let i = 0; i <= 10; i++) {
     for (let j = 0; j <= 5; j++) {
       if (gameState[i] && gameState[i][j]) {
+        // if (
+        //   /* case 1: $_$ */ i + 1 <= 10 &&
+        //   i + 1 >= 0 &&
+        //   !gameState[i + 1][j] &&
+        //   gameState[i + 2] &&
+        //   gameState[i + 2][j] === gameState[i][j]
+        // ) {
+        //   console.log("third horizontal 1");
+
+        //   selectedColumnIndex = i + 1;
+        //   return selectedColumnIndex;
+        // } else if (
+        //   /* case 2: $_$ */ i - 1 <= 10 &&
+        //   i - 1 >= 0 &&
+        //   !gameState[i - 1][j] &&
+        //   gameState[i - 2] &&
+        //   gameState[i - 2][j] === gameState[i][j]
+        // ) {
+        //   console.log("third horizontal 2");
+
+        //   selectedColumnIndex = i - 1;
+        //   return selectedColumnIndex;
+        // }
+
         if (
-          /* case 1: $_$ */ i + 1 <= 10 &&
-          i + 1 >= 0 &&
-          !gameState[i + 1][j] &&
-          gameState[i + 2] &&
-          gameState[i + 2][j] === gameState[i][j]
-        ) {
-          console.log("third horizontal 1");
-
-          selectedColumnIndex = i + 1;
-          return selectedColumnIndex;
-        } else if (
-          /* case 2: $_$ */ i - 1 <= 10 &&
-          i - 1 >= 0 &&
-          !gameState[i - 1][j] &&
-          gameState[i - 2] &&
-          gameState[i - 2][j] === gameState[i][j]
-        ) {
-          console.log("third horizontal 2");
-
-          selectedColumnIndex = i - 1;
-          return selectedColumnIndex;
-        } else if (
           /* case 3: $__$ */ i + 1 <= 10 &&
           i + 1 >= 0 &&
           !gameState[i + 1][j] &&
@@ -47,18 +49,28 @@ export const horizontalOne = gameState => {
           selectedColumnIndex = i - 1;
           return selectedColumnIndex;
         } else if (
-          /* case 5: $ */ i + 1 <= 10 &&
+          /* case 5: $___ */ i + 1 <= 10 &&
           i + 1 >= 0 &&
-          !gameState[i + 1][j]
+          gameState[i + 1] &&
+          !gameState[i + 1][j] &&
+          gameState[i + 2] &&
+          !gameState[i + 2][j] &&
+          gameState[i + 3] &&
+          !gameState[i + 3][j]
         ) {
           console.log("third horizontal 5");
 
           selectedColumnIndex = i + 1;
           return selectedColumnIndex;
         } else if (
-          /* case 5: $ */ i - 1 <= 10 &&
+          /* case 5: ___$ */ i - 1 <= 10 &&
           i - 1 >= 0 &&
-          !gameState[i - 1][j]
+          gameState[i - 1] &&
+          !gameState[i - 1][j] &&
+          gameState[i - 2] &&
+          !gameState[i - 2][j] &&
+          gameState[i - 3] &&
+          !gameState[i - 3][j]
         ) {
           console.log("third horizontal 6");
 
